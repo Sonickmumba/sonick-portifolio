c40a6887f52758d7afbdc023fa8a5888eff98e4c
Sonick Mumba <kingellie.mumba@gmail.com>
2023-12-24 21:15:48 +0200

    Add button with tailwind css

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -23,12 +23,13 @@ const Home = () => {
               </button> */}
               <button
                 type="button"
-                className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                className="bg-gradient-to-r from-violet-500 to-sky-500 rounded-lg xxxs:px-5 xxxs:text-lg xxs:px-5 xxs:text-xl xs:px-5 sm:px-5 xs:text-xl sm:text-xl py-2 shadow-lg hover:shadow-xl hover:bg-gray-600 hover:text-white transition-all duration-200 ease hover:bg-gradient-to-l "
+                // className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 id="#about"
               >
                 Hire me
               </button>
-              <a href="" className="download">
+              <a href="" className="download text-blue-600 cursor-pointer">
                 Download CV
                 <i className="fa-solid fa-download" id="download-icon"></i>
               </a>

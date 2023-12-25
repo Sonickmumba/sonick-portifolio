813344f7a0759b14fe1b4fdd66b0ccdddaf5e467
Sonick Mumba <kingellie.mumba@gmail.com>
2023-12-25 18:10:04 +0200

    Update links on the overlay to display the clicked section

--- src/components/header/Overlay.jsx
+++ src/components/header/Overlay.jsx
@@ -5,9 +5,15 @@ const Overlay = ({onResetHamburger}) => {
   const [overlay, setOverlay] = useState(true);
 
   const handleCloseOverlay = () => {
-    setOverlay((prevOverlay) => !prevOverlay);
+    // setOverlay((prevOverlay) => !prevOverlay);
+    setOverlay(false);
     onResetHamburger();
   }
+
+  const handleLinkClick = () => {
+    handleCloseOverlay();
+  };
+  
 
   return (
     <div
@@ -19,6 +25,7 @@ const Overlay = ({onResetHamburger}) => {
           <a
             href="/#home"
             className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            onClick={handleLinkClick}
           >
             Home
           </a>
@@ -27,6 +34,7 @@ const Overlay = ({onResetHamburger}) => {
           <a
             href="/#about"
             className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            onClick={handleLinkClick}
           >
             About
           </a>
@@ -35,6 +43,7 @@ const Overlay = ({onResetHamburger}) => {
           <a
             href="/#projects"
             className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            onClick={handleLinkClick}
           >
             Projects
           </a>
@@ -43,6 +52,7 @@ const Overlay = ({onResetHamburger}) => {
           <a
             href="/#tech"
             className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            onClick={handleLinkClick}
           >
             Technologies
           </a>
@@ -51,6 +61,7 @@ const Overlay = ({onResetHamburger}) => {
           <a
             href="/#contact"
             className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            onClick={handleLinkClick}
           >
             Contact
           </a>

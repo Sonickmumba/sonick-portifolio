cf53a49c8e7b5e429d489438086bd934134a1701
Sonick Mumba <kingellie.mumba@gmail.com>
2023-12-26 01:45:00 +0200

    Remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Header from "./components/header/Header";
 import Home from "./components/main/Home";
 import Project from "./components/projects/Project";

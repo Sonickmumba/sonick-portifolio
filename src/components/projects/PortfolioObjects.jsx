debab86f0e0a8da834b57259faff30e0954e94f8
Sonick Mumba <kingellie.mumba@gmail.com>
2023-12-29 10:09:19 +0200

    Add links for the images in the project section

--- src/components/projects/PortfolioObjects.jsx
+++ src/components/projects/PortfolioObjects.jsx
@@ -9,7 +9,8 @@ const portfolioObjects = [
       'Web application based on an external API. We selected an API that provides data about Meals. One is able to like and comment on a meal.',
     liveUrl: 'https://megha-n-bodke.github.io/CapstoneProject2/dist/',
     sourceUrl: 'https://github.com/Sonickmumba/CapstoneProject2',
-    image: 'https://github.com/Sonickmumba/sonick-portifolio/assets/106140591/d1164c4a-9ee2-47d1-bdce-6a5aad279530',
+    // image: 'https://github.com/Sonickmumba/sonick-portifolio/assets/106140591/d1164c4a-9ee2-47d1-bdce-6a5aad279530',
+    image: 'https://res.cloudinary.com/djlpfihyu/image/upload/v1703835645/samples/breakfast.jpg',
   },
 
   {
@@ -21,7 +22,8 @@ const portfolioObjects = [
       'Vesper application for all lovers of cars. Let your car make money for you by listing it on our platform so others can view and rent.',
     liveUrl: 'https://vesper-one.vercel.app/',
     sourceUrl: 'https://github.com/Sonickmumba/vesper-frontend',
-    image: 'https://github.com/Sonickmumba/sonick-portifolio/assets/106140591/494236af-2188-43b1-9d23-6f2ed17dbf1d',
+    // image: 'https://github.com/Sonickmumba/sonick-portifolio/assets/106140591/494236af-2188-43b1-9d23-6f2ed17dbf1d',
+    image: 'https://res.cloudinary.com/djlpfihyu/image/upload/v1703836964/Vesper.jpeg_ohkg8z.png'
   },
 
   {
@@ -33,7 +35,7 @@ const portfolioObjects = [
       'Single Page App for math calculations & reading random math related quotes, offering quick access to simple tools and resources',
     liveUrl: 'https://dancing-conkies-c08f95.netlify.app/',
     sourceUrl: 'https://github.com/Sonickmumba/math-magicians',
-    image: 'https://github.com/Sonickmumba/sonick-portifolio/assets/106140591/d88aa174-5636-4154-99ea-53fbe24cb6fc',
+    image: 'https://res.cloudinary.com/djlpfihyu/image/upload/v1703836893/MathMagician.jpeg_vt1b8a.png',
   },
   {
     id: 6,
@@ -44,7 +46,7 @@ const portfolioObjects = [
       'Zambia Mathematics Association Conference website with info on dates, speakers, tickets, location. Impact of Mathematics in a society.',
     liveUrl: 'https://sonickmumba.github.io/Capstone-Project-1/',
     sourceUrl: 'https://github.com/Sonickmumba/Capstone-Project-1',
-    image: 'https://github.com/Sonickmumba/sonick-portifolio/assets/106140591/a6226b1a-bfb4-4d32-a960-cbc58aa3b263',
+    image: 'https://res.cloudinary.com/djlpfihyu/image/upload/v1703836912/2022_ZAMSA_Conference.jpeg_gtahy0.png',
   },
 ];
 
